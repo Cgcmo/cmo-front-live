6ef892ffa439f06695629be6ae1a0c895ae583ff
Cgcmo <cmocg4857@gmail.com>
2025-04-01 00:08:52 +0530

    Update and rename SearchResultsComponent.js to page.js

---
+++ src/app/search-results/page.js
@@ -0,0 +1,21 @@
+// app/search-results/page.jsx
+import { Suspense } from "react";
+import Navbar from "../dashboard/components/Navbar";
+import Footer from "../dashboard/components/Footer";
+import dynamic from "next/dynamic";
+
+const SearchResultsComponent = dynamic(() => import("../../components/SearchResultsComponent"), {
+  ssr: false,
+});
+
+export default function SearchResults() {
+  return (
+    <div className="min-h-screen bg-white relative pb-24">
+      <Navbar />
+      <Suspense fallback={<p className="text-center mt-10">Loading search results...</p>}>
+        <SearchResultsComponent />
+      </Suspense>
+      <Footer />
+    </div>
+  );
+}
